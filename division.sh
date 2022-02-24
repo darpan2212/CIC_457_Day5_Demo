27ec69082988ab30fb3bd371866c5023160a839c
Darpan Madlani <darpan.madlani@gmail.com>
2022-02-24 12:05:05 +0530

    Update division.sh
    both type of division performed

--- division.sh
+++ division.sh
@@ -1,8 +1,8 @@
 read -p "Enter the number a : " a;
 read -p "Enter the number b : " b;
 
-#c=$(($a/$b));
 c=`echo $a $b | awk '{print $1/$2}'`
-
-
 echo $a/$b=$c;
+
+c=$(($a/$b));
+echo "normal division $a/$b=$c";
